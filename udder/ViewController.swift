c033de40f61cf85d1b37106eb814d6f96fe03f23
Lin <banelin30@gmail.com>
2023-10-02 14:07:53 +0800

    Initial Commit

---
+++ udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  udder
+//
+//  Created by 林佑淳 on 2023/10/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
